b493bd249c57f621169fac25c9327d667d1649b8
Adarsh Tiwari <93669742+adarshiert@users.noreply.github.com>
2022-04-21 19:00:47 +0530

    Update WeatherAppusingpython.py

--- WeatherAppusingpython.py
+++ WeatherAppusingpython.py
@@ -5,7 +5,7 @@ import time
 
 def getWeather(canvas):
     city = textField.get()
-    api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=06c921750b9a82d8f5d1294e1586276f"
+    api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=your api id"
 
     json_data = requests.get(api).json()
     condition = json_data['weather'][0]['main']
